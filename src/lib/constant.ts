7d8ddc5aff363f0c6aad0f09b2f32c2fdc316fd2
LiusDev <quydx.work@gmail.com>
2025-09-22 16:59:35 +0700

    fix: be url

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,2 +1,2 @@
-// export const BE_URL = "https://sale-bot-be.tuanyenbai.id.vn/api"
-export const BE_URL = "http://localhost:8787/api"
+export const BE_URL = "https://sale-bot-be.tuanyenbai.id.vn/api"
+// export const BE_URL = "http://localhost:8787/api"
